ac814999abbb917b0e0f2183e49aab7f1c9b8df1
Lewis Godowski <lewis.godowski@bcgdv.com>
2023-08-05 15:05:31 -0700

    add point from theo. move structure extension

--- Sources/Anderson/Extensions/Structure+.swift
+++ Sources/Anderson/Extensions/Structure+.swift
@@ -1,3 +1,15 @@
 import PackStream
+import Theo
+
+public extension Structure {
+  var subType: ResponseItem.Type? {
+    if signature == 78, items.count >= 3 { return Node.self }
+    if signature == 80, items.count >= 3 { return Path.self }
+    if signature == 88, items.count >= 3 { return Point.self }
+    if signature == 82, items.count >= 5 { return Relationship.self }
+    if signature == 114, items.count >= 3 { return UnboundRelationship.self }
+    return nil
+  }
+}
 
 extension Structure: @unchecked Sendable { }
